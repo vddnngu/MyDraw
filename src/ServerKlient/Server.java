2787265fb41e1a92998ee47189b00a593c005183
jookseme <starwarsrk@mail.ru>
2019-11-09 18:31:55 +0300

    init commit

--- src/ServerKlient/Server.java
+++ src/ServerKlient/Server.java
@@ -1,4 +1,16 @@
 package ServerKlient;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Server {
+    int port = 1111;
+    ServerSocket ss;
+    List<Socket> cs;
+    List <DataInputStream> dis;
+    List <DataOutput> dos;
+    Socket sc;
+    //работать буду завтра, пока прогресс огромный!!!!!
 }
