6b9fd0d5a474540bf885426f7fa8d5e319069ee7
dvolkov <vdddzer152@yandex.ru>
2019-11-11 12:30:16 +0300

    Added implementation of classes for Support

--- src/TopoGeomAPI/Support/Ax2d.java
+++ src/TopoGeomAPI/Support/Ax2d.java
@@ -1,4 +1,32 @@
 package TopoGeomAPI.Support;
 
+import TopoGeomAPI.Geometry.Point;
+
 public class Ax2d {
+    //States-------------------------------------------------------------------
+
+    private Direction myXDir;
+    private Direction myYXir;
+    private Point myCenter;
+
+    //Methods------------------------------------------------------------------
+
+    public Ax2d(Point centet, Direction yDir){
+        myCenter = centet;
+        myYXir = yDir;
+        //TODO Calculate xDir!
+    }
+
+    public void Move(Direction dir, double distance){
+        myCenter.X+=dir.getCords().X*distance;
+        myCenter.Y+=dir.getCords().Y*distance;
+    }
+
+    public Ax2d Moved(Direction dir, double distance){
+        Point newCenter = new Point(
+        myCenter.X+dir.getCords().X*distance,
+        myCenter.Y+dir.getCords().Y*distance);
+        return new Ax2d(newCenter, myYXir);
+
+    }
 }
--- src/TopoGeomAPI/Support/Direction.java
+++ src/TopoGeomAPI/Support/Direction.java
@@ -1,4 +1,27 @@
 package TopoGeomAPI.Support;
 
+import TopoGeomAPI.Geometry.Point;
+
 public class Direction {
+    //States-------------------------------------------------------------------
+
+    private double X,Y;
+
+    //Methods------------------------------------------------------------------
+
+    public Direction(double x, double y) {
+        X = x;
+        Y = y;
+        Normalize();
+    }
+
+    public Point getCords(){
+        return new Point(X,Y);
+    }
+
+    private void Normalize(){
+        double len = Math.sqrt(X*X+Y*Y);
+        X/=len;
+        Y/=len;
+    }
 }
