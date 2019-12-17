f99ce5b501712639991035745f14778ef7919f13
dvolkov <vdddzer152@yandex.ru>
2019-12-17 14:56:20 +0300

    use jdk 13

---
+++ src/ServerClient/DataTransferType.java
@@ -0,0 +1,9 @@
+package ServerClient;
+
+public class DataTransferType {
+    String data;
+    public DataTransferType(){
+
+    }
+    public void trasfer(Shape)
+}
--- src/ServerClient/Server.java
+++ src/ServerClient/Server.java
@@ -1,5 +1,6 @@
 package ServerClient;
 
+import java.lang.reflect.Type;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
@@ -9,96 +10,99 @@ import java.io.DataOutputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
+class MessagesThread extends  Thread {
+    public  int id;
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String answer = dataInputStreams.get(id).readUTF();
+                System.out.println(id);
+                System.out.println(answer);
+                sendMessages(answer);
+            } catch (java.io.IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+    public void sendMessages(String answer)
+    {
+        int idClient;
+        for (idClient = 0; idClient < socketList.size(); idClient++) {
+            try {
+                dataOutputStreams.get(idClient).writeUTF(answer);
+                dataOutputStreams.get(idClient).flush();
+            } catch (IOException ex) {
+                //ex.printStackTrace();
+            }
+        }
+    }
+}
 
+class ReaderThread extends  Thread {
+    public  int id;
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String answer = dataInputStreams.get(id).readUTF();
+                System.out.println(id);
+                System.out.println(answer);
+
+            } catch (java.io.IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+}
+class WaitClientTread extends Thread {
+    @Override
+    public void run() {
+        try {
+            Socket socket = serverSocket.accept();
+            synchronized (socketList) {
+                int id = socketList.size();
+                System.out.println("Connected: " + id + " client!");
+                socketList.add(socket);
+                DataInputStream newDis = new DataInputStream(socket.getInputStream());
+                DataOutputStream newDos = new DataOutputStream(socket.getOutputStream());
+                MessagesThread reader = new MessagesThread();
+                reader.id = id;
+                synchronized (dataInputStreams) {
+                    dataInputStreams.add(newDis);
+                    synchronized (dataOutputStreams) {
+                        dataOutputStreams.add(newDos);
+                        synchronized (readers) {
+                            readers.add(reader);
+                        }
+                    }
+                }
+                reader.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MyThread nextThread = new MyThread();
+        nextThread.start();
+    }
+}
 public class Server {
     //States
     int port = 1111;
     InetAddress inetAddress = null;
     ServerSocket serverSocket;
-    List<Socket> socketList;
-    List<DataInputStream> dataInputStreams;
-    List<DataOutputStream> dataOutputStreams;
+
+    List<String> modelsForDrawing;
+    List<String> customModels;
+
+
 
     List<MessagesThread> readers;
 
     //Methods
-    class MessagesThread extends  Thread {
-        public  int id;
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    String answer = dataInputStreams.get(id).readUTF();
-                    System.out.println(id);
-                    System.out.println(answer);
-                    sendMessages(answer);
-                } catch (java.io.IOException ex) {
-                    ex.printStackTrace();
-                    break;
-                }
-            }
-        }
-        public void sendMessages(String answer)
-        {
-            int idClient;
-            for (idClient = 0; idClient < socketList.size(); idClient++) {
-                try {
-                    dataOutputStreams.get(idClient).writeUTF(answer);
-                    dataOutputStreams.get(idClient).flush();
-                } catch (IOException ex) {
-                    //ex.printStackTrace();
-                }
-            }
-        }
-    }
 
-    class ReaderThread extends  Thread {
-        public  int id;
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    String answer = dataInputStreams.get(id).readUTF();
-                    System.out.println(id);
-                    System.out.println(answer);
-                } catch (java.io.IOException ex) {
-                    ex.printStackTrace();
-                    break;
-                }
-            }
-        }
-    }
-    class MyThread extends Thread {
-        @Override
-        public void run() {
-            try {
-                Socket socket = serverSocket.accept();
-                synchronized (socketList) {
-                    int id = socketList.size();
-                    System.out.println("Connected: " + id + " client!");
-                    socketList.add(socket);
-                    DataInputStream newDis = new DataInputStream(socket.getInputStream());
-                    DataOutputStream newDos = new DataOutputStream(socket.getOutputStream());
-                    MessagesThread reader = new MessagesThread();
-                    reader.id = id;
-                    synchronized (dataInputStreams) {
-                        dataInputStreams.add(newDis);
-                        synchronized (dataOutputStreams) {
-                            dataOutputStreams.add(newDos);
-                            synchronized (readers) {
-                                readers.add(reader);
-                            }
-                        }
-                    }
-                    reader.start();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            MyThread nextThread = new MyThread();
-            nextThread.start();
-        }
-    }
 
     public void startServer() {
         try {
